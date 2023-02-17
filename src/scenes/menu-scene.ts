fc4bf72bf42fa244baba86e34bf2cf87d6e77e09
Ramil Zaynetdinov <ramil.zv@gmail.com>
2023-02-18 00:31:54 +0300

    created dw and first level

---
+++ src/scenes/menu-scene.ts
@@ -0,0 +1,41 @@
+export class MenuScene extends Phaser.Scene {
+  private startKey: Phaser.Input.Keyboard.Key
+  private bitmapTexts: Phaser.GameObjects.BitmapText[] = []
+
+  constructor() {
+    super({
+      key: 'MenuScene',
+    })
+  }
+
+  init(): void {
+    this.startKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+    this.startKey.isDown = false
+    this.initGlobalDataManager()
+  }
+
+  create(): void {
+    this.add.image(0, 0, 'background').setOrigin(0, 0)
+
+    this.bitmapTexts.push(this.add.bitmapText(this.sys.canvas.width / 2 - 65, 30, 'font', 'Darkwing Duck', 10))
+    this.bitmapTexts.push(this.add.bitmapText(this.sys.canvas.width / 2 - 62, 105, 'font', 'Press S to START', 8))
+  }
+
+  update(): void {
+    if (this.startKey.isDown) {
+      this.scene.start('HUDScene')
+      this.scene.start('GameScene')
+      this.scene.bringToTop('HUDScene')
+    }
+  }
+
+  private initGlobalDataManager(): void {
+    this.registry.set('time', 400)
+    this.registry.set('level', 'level_1')
+    this.registry.set('timeLabel', 'TIME')
+    this.registry.set('score', 0)
+    this.registry.set('coins', 0)
+    this.registry.set('lives', 2)
+    this.registry.set('spawn', { x: 16, y: 44, dir: 'down' })
+  }
+}
