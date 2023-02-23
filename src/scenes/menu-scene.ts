1fb7a4b4ec9630d3f609c33deda3b14332ee21d3
Ramil Zaynetdinov <ramil.zv@gmail.com>
2023-02-23 12:48:40 +0300

    added girl names and logic

--- src/scenes/menu-scene.ts
+++ src/scenes/menu-scene.ts
@@ -17,7 +17,21 @@ export class MenuScene extends Phaser.Scene {
   create(): void {
     this.add.image(0, 0, 'background').setOrigin(0, 0)
 
-    this.bitmapTexts.push(this.add.bitmapText(this.sys.canvas.width / 2 - 65, 30, 'font', 'Darkwing Duck', 10))
+    const text1 = this.add.text(5, 5, 'ОТ МАЛЬЧИКОВ\nЛМС/ПЛАТФОРМЫ', { fontFamily: 'coralWaves' })
+    text1.setFontSize(20)
+    text1.setColor('#0000f8')
+    const text2 = this.add.text(this.sys.canvas.width / 2, 42, 'Спасибо!', { fontFamily: 'coralWaves' })
+    text2.setFontSize(28)
+    text2.setColor('#f80000')
+    const text3 = this.add.text(
+      5,
+      63,
+      'В честь 8 марта\nпоздравляем всех девочек,\nкто работает над\nпроектами Деловой Среды',
+      { fontFamily: 'coralWaves' },
+    )
+    text3.setFontSize(20)
+    text3.setColor('#f04867')
+
     this.bitmapTexts.push(this.add.bitmapText(this.sys.canvas.width / 2 - 62, 105, 'font', 'Press S to START', 8))
   }
 
